6cfb6b741f306615823fa9b32b3d726453ee3288
RamonGuy <Ramonguy@yahoo.com>
2022-09-22 16:09:12 -0400

    Created game class and display welcome method

---
+++ game.py
@@ -0,0 +1,10 @@
+from human import Human
+from ai import AI
+class Game():
+    def __init__(self) -> None:
+        pass
+
+    def display_welcome(self):
+        print("Hello welcome to the RPSLS game!!! ")
+
+    
