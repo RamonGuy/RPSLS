612f701bce595068431ea269c79920ce484ac841
RamonGuy <Ramonguy@yahoo.com>
2022-09-22 15:49:19 -0400

    Created my human_gesture method for human class

--- human.py
+++ human.py
@@ -4,3 +4,8 @@ class Human(Player):
         super().__init__(name)
         self.score = 0
         
+    #I need to come back to this and figure out what to do with this code 
+    def human_gesture(self):
+        self.gesture_list = ["Rock", 'Paper', 'Scissor', 'Lizard', 'Spock']
+        choose_gesture = input(f'Please choose your gesture')
+        print(f'{self.name} has chosen {choose_gesture}')
