078a3ba607c9d1ce6475af8a146f2292e1938c73
RamonGuy <Ramonguy@yahoo.com>
2022-09-24 19:45:55 -0400

    Created my player_choice function for the game_cls

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 from human import Human
 from ai import AI
-class Game():
+player1 = Human('Human')
+player2 = AI('AI')
+class Game:
     def __init__(self) -> None:
         pass
 
@@ -18,4 +20,24 @@ class Game():
         print('Paper disproves Spock')
         print('Spock vaporizes Rock')
         print('Rock crushes Scissors')
+
+    def player_choice(self):
+       input("How many players will ne playing? 1, 2, or 3 for a surprise!!!!! ")
+       if input == 1:
+            player1.human_gesture()
+            player2.ai_gesture()
+       elif input == 2:
+            player1.human_gesture()
+            player1.human_gesture
+       elif input == 3:
+            player2.ai_gesture
+            player2.ai_gesture
+       else:
+            print("Please type in a correct number")
+        
+            
+        
+            
+        
+        
 
